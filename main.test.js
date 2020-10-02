c7929e71b4ea77be5a103a67eccafa1b450fd889
frani1 <franinnocenti@gmail.com>
2020-10-02 12:58:36 -0300

    update test with diff languages

--- main.test.js
+++ main.test.js
@@ -1,5 +1,13 @@
 const getRandomFruitsName = require('./main');
 
-test('Get fruits name text', () => {
+test('Get fruits name text with default name', () => {
   expect(getRandomFruitsName()).toBeTruthy();
+});
+
+test('Get fruits name text in English', () => {
+  expect(getRandomFruitsName('en')).toBeTruthy();
+});
+
+test('Get fruits name text in Spanish', () => {
+  expect(getRandomFruitsName('es')).toBeTruthy();
 });
