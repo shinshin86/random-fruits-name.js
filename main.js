1a821e9e8047acae75575ac80f52894a8953da41
shinshin86 <beagles1986@gmail.com>
2020-10-04 10:01:59 +0900

    Add Japanese language

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const enFruits = require('./en.json');
 const esFruits = require('./es.json');
+const jaFruits = require('./ja.json');
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const getRandomFruitsName = (language = 'en') => {
   let fruits = enFruits.fruits;
   if (language === 'es') fruits = esFruits.fruits;
+  if (language === 'ja') fruits = jaFruits.fruits;
   return fruits[getRandomInt(fruits.length)];
 };
 
