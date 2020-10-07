e08f9a96d1e0a4ef7e58b632226dd4b4ff860f88
frani1 <franinnocenti@gmail.com>
2020-10-07 16:40:58 -0300

    add Code Comment

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const ptFruits = require('./langs/pt.json');
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
+/**
+ * Get Random Fruit Name
+ * @param {string} language - choose  language of random fruit, default is english: 'en', function ue [ISO-639-1 codes](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
+ * @returns {string} return a string with fruits name, example: 'Apple'
+ */
 const getRandomFruitsName = (language = 'en') => {
   let fruits = enFruits.fruits;
   if (language === 'es') fruits = esFruits.fruits;
