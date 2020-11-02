96d80568de06010f68dd7e17d723d7fa388e04e3
shinshin86 <beagles1986@gmail.com>
2020-11-02 14:27:27 +0900

    Add maxWords option test

--- main.test.js
+++ main.test.js
@@ -1,21 +1,61 @@
 const getRandomFruitsName = require('./main')
 
-test('Get fruits name text with default name', () => {
-  expect(getRandomFruitsName()).toBeTruthy()
-})
+describe('random-fruits-name', () => {
+  describe('Basic usage', () => {
+    test('Get fruits name text with default name', () => {
+      expect(getRandomFruitsName()).toBeTruthy()
+    })
 
-test('Get fruits name text in English', () => {
-  expect(getRandomFruitsName('en')).toBeTruthy()
-})
+    test('Get fruits name text in English', () => {
+      expect(getRandomFruitsName('en')).toBeTruthy()
+    })
 
-test('Get fruits name text in Spanish', () => {
-  expect(getRandomFruitsName('es')).toBeTruthy()
-})
+    test('Get fruits name text in Spanish', () => {
+      expect(getRandomFruitsName('es')).toBeTruthy()
+    })
 
-test('Get fruits name text in Japanese', () => {
-  expect(getRandomFruitsName('ja')).toBeTruthy()
-})
+    test('Get fruits name text in Japanese', () => {
+      expect(getRandomFruitsName('ja')).toBeTruthy()
+    })
 
-test('Get fruits name text in Portuguese', () => {
-  expect(getRandomFruitsName('pt')).toBeTruthy()
+    test('Get fruits name text in Portuguese', () => {
+      expect(getRandomFruitsName('pt')).toBeTruthy()
+    })
+  })
+
+  describe('maxWords option', () => {
+    describe('If `maxWords: 1` is specified.', () => {
+      test('Select English', () => {
+        const fruitsName = getRandomFruitsName('en', { maxWords: 1 })
+        expect(fruitsName).toBeTruthy()
+
+        // `fruitsName` should  hasn't space
+        expect(fruitsName.split(' ').length).toEqual(1)
+      })
+
+      test('Select Spanish', () => {
+        const fruitsName = getRandomFruitsName('es', { maxWords: 1 })
+        expect(fruitsName).toBeTruthy()
+
+        // `fruitsName` should  hasn't space
+        expect(fruitsName.split(' ').length).toEqual(1)
+      })
+
+      test('Select Japanese', () => {
+        const fruitsName = getRandomFruitsName('ja', { maxWords: 1 })
+        expect(fruitsName).toBeTruthy()
+
+        // `fruitsName` should  hasn't space
+        expect(fruitsName.split(' ').length).toEqual(1)
+      })
+
+      test('Select Portuguese', () => {
+        const fruitsName = getRandomFruitsName('pt', { maxWords: 1 })
+        expect(fruitsName).toBeTruthy()
+
+        // `fruitsName` should  hasn't space
+        expect(fruitsName.split(' ').length).toEqual(1)
+      })
+    })
+  })
 })
