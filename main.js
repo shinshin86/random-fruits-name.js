b2e0ee4e9ccc7162cea35cd252192e305b4281c8
shinshin86 <beagles1986@gmail.com>
2020-11-02 14:22:05 +0900

    Add max words option

--- main.js
+++ main.js
@@ -5,16 +5,31 @@ const ptFruits = require('./langs/pt.json')
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max))
 
+const filterMaxWords = (fruits, maxWords) => {
+  if (!Number.isInteger(maxWords))
+    throw new Error('The value of maxWords is incorrect.')
+
+  // If `maxWords` is 1, no space is allowed in the string.
+  const allowSpaceCount = maxWords - 1
+
+  const filteredFruits = fruits.filter((f) => {
+    return f.split(' ').length - 1 <= allowSpaceCount
+  })
+  return filteredFruits
+}
+
 /**
  * Get Random Fruit Name
  * @param {string} language - choose  language of random fruit, default is english: 'en', function ue [ISO-639-1 codes](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
  * @returns {string} return a string with fruits name, example: 'Apple'
  */
-const getRandomFruitsName = (language = 'en') => {
+const getRandomFruitsName = (language = 'en', option = {}) => {
   let fruits = enFruits.fruits
   if (language === 'es') fruits = esFruits.fruits
   if (language === 'ja') fruits = jaFruits.fruits
   if (language === 'pt') fruits = ptFruits.fruits
+
+  if (option.maxWords) fruits = filterMaxWords(fruits, option.maxWords)
   return fruits[getRandomInt(fruits.length)]
 }
 
